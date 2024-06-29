257e2ead801dd49a7fc20f536caf152cfbd54738
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-30 00:45:37 +0200

    Add labels in AmountWithCurrencyInput

--- src/components/AmountWithCurrencyInput/AmountWithCurrencyInput.tsx
+++ src/components/AmountWithCurrencyInput/AmountWithCurrencyInput.tsx
@@ -4,7 +4,13 @@ import styles from "./AmountWithCurrencyInput.module.scss";
 
 export default function AmountWithCurrencyInput() {
     return <div className={styles.amountWithCurrency}>
-        <AmountInput />
-        <CurrencyInput />
+        <div className={styles.amountInput}>
+            <label>Kwota</label>
+            <AmountInput />
+        </div>
+        <div className={styles.currencyInput}>
+            <label>Waluta</label>
+            <CurrencyInput />
+        </div>
     </div>
 }
