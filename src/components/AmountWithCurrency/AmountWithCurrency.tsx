86d6da61c48fe7d1ab4919cfe4eace12cfc982f0
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-30 01:11:34 +0200

    Disable AmountInput according to type in AmountWithCurrency

--- src/components/AmountWithCurrency/AmountWithCurrency.tsx
+++ src/components/AmountWithCurrency/AmountWithCurrency.tsx
@@ -2,11 +2,15 @@ import CurrencyInput from "../CurrencyInput/CurrencyInput.tsx";
 import AmountInput from "../AmountInput/AmountInput.tsx";
 import styles from "./AmountWithCurrencyInput.module.scss";
 
-export default function AmountWithCurrencyInput() {
+interface AmountWithCurrencyProps {
+    type: "input" | "output";
+}
+
+export default function AmountWithCurrency(props: AmountWithCurrencyProps) {
     return <div className={styles.amountWithCurrency}>
         <div className={styles.amountInput}>
             <label>Kwota</label>
-            <AmountInput />
+            <AmountInput disabled={props.type == "output"} />
         </div>
         <div className={styles.currencyInput}>
             <label>Waluta</label>
