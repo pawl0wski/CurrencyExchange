c30cfd737e60c16777ecb83ae366f3920c66cc52
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-29 19:31:03 +0200

    Add sass

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import store from "./store.ts"
 import {Provider} from "react-redux";
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
