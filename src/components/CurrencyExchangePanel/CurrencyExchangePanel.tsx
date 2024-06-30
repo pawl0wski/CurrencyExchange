6578ca0c931c67a38697444c5dbae75fc2e4261d
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-30 02:08:59 +0200

    Modify CurrencyExchangePanel content and styles

--- src/components/CurrencyExchangePanel/CurrencyExchangePanel.tsx
+++ src/components/CurrencyExchangePanel/CurrencyExchangePanel.tsx
@@ -1,8 +1,15 @@
-import styles from "./CurrencyExchangePanel.module.scss"
-import AmountWithCurrencyInput from "../AmountWithCurrencyInput/AmountWithCurrencyInput.tsx";
+import styles from "./CurrencyExchangePanel.module.scss";
+import AmountWithCurrency from "../AmountWithCurrency/AmountWithCurrency.tsx";
 
 export default function CurrencyExchangePanel() {
     return <div className={styles.currencyExchangePanel}>
-        <AmountWithCurrencyInput />
-    </div>
+        <h3>Waluta źródłowa</h3>
+        <AmountWithCurrency type={"input"} />
+        <h3>Waluta docelowa</h3>
+        <AmountWithCurrency type={"output"} />
+        <hr className={styles.buttonSeparator} />
+        <button className={styles.calculateButton} >
+            Oblicz
+        </button>
+    </div>;
 }
