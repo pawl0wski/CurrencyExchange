a64ce0a8a7199906fdf22786b3b50e9f8a8e4deb
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-30 11:06:09 +0200

    Improve flagService by adding FlagApiResponse

--- src/services/flagService.ts
+++ src/services/flagService.ts
@@ -1,9 +1,17 @@
+interface FlagApiResponse {
+    flags: {
+        png: string
+    }
+}
+
 export async function getFlagUrl(currencyCode: string) : Promise<string> {
     const response = await fetch(`https://restcountries.com/v3.1/currency/${currencyCode}`)
 
     if (response.ok) {
-        const apiResponse : {flags: {png: string}}[] =  await response.json()
-        return apiResponse[0].flags.png;
+        const apiResponses : FlagApiResponse[] =  await response.json()
+        const firstApiResponse = apiResponses[0];
+
+        return firstApiResponse.flags.png;
     }
 
     throw new Error(response.statusText);
