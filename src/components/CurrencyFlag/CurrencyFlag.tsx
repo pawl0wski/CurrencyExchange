9b4a001e72e7de6b908a465135449d8a38e78737
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-30 02:07:34 +0200

    Add CurrencyFlag component

---
+++ src/components/CurrencyFlag/CurrencyFlag.tsx
@@ -0,0 +1,24 @@
+import { getFlagUrl } from "../../services/flagService.ts";
+import { useEffect, useState } from "react";
+import { MdQuestionMark } from "react-icons/md";
+import styles from "./CurrencyFlag.module.scss";
+
+interface CurrencyFlagProps {
+    currencyCode: string
+}
+
+export default function CurrencyFlag(props: CurrencyFlagProps) {
+    const [flagUrl, setFlagUrl] = useState<string | null>(null);
+
+    useEffect(() => {
+        getFlagUrl(props.currencyCode).then(r => setFlagUrl(r));
+    }, []);
+
+    return <div className={styles.currencyFlag}>
+        {
+            flagUrl === null ?
+                <MdQuestionMark />
+                : <img src={flagUrl} alt={props.currencyCode} />
+        }
+    </div>
+}
