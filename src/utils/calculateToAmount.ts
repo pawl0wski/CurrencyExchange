f8f4bb63697696113a6b326795bdb91852d4ec64
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-07-01 20:40:05 +0200

    Improve precission in calculateToAmount

--- src/utils/calculateToAmount.ts
+++ src/utils/calculateToAmount.ts
@@ -1,5 +1,6 @@
 import CurrencyRate from "@/types/currencyRate.ts";
 import Currency from "@/types/currency.ts";
+import Decimal from "decimal.js";
 
 export function calculateToAmount(fromAmount: number, fromCurrency: Currency, toCurrency: Currency, currencyRates: CurrencyRate[]): number {
     const fromCurrencyRate = findCurrencyRate(fromCurrency, currencyRates);
@@ -8,14 +9,10 @@ export function calculateToAmount(fromAmount: number, fromCurrency: Currency, to
     if (fromCurrencyRate === null || toCurrencyRate === null)
         return 0;
 
-    const amount = (fromAmount / fromCurrencyRate.rate) * toCurrencyRate.rate;
-    return roundAmountToTwoDigits(amount);
+    const amount = new Decimal(fromAmount).dividedBy(fromCurrencyRate.rate).mul(toCurrencyRate.rate);
+    return parseFloat(amount.toFixed(2));
 }
 
 function findCurrencyRate(currency: Currency, currencyRates: CurrencyRate[]): CurrencyRate | null {
     return currencyRates.find(cr => cr.code == currency.code) ?? null;
-}
-
-function roundAmountToTwoDigits(amount: number): number {
-    return Math.floor(amount * 100) / 100
 }
